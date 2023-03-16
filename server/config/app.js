6269c8c857ffd1cea66af716c054328c44c988b9
Josh H <josh.terance@gmail.com>
2023-03-16 11:19:02 -0400

    initial server setup

---
+++ server/config/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Use the 'body-parser' middleware functions
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// setup CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
+// Export the app
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
+const app = require('./config/app.js');
 const port = 5000;
+
+app.listen(port);
+console.log(`Server running on port ${port}`);
+
+module.exports = app;
