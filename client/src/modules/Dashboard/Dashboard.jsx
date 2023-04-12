14d78a1db41ab9435857f70cf8fc485f8f566b52
vaniyke <114178845+vaniyke@users.noreply.github.com>
2023-04-12 10:20:53 +0000

    add dashboard tabs

--- client/src/modules/Dashboard/Dashboard.jsx
+++ client/src/modules/Dashboard/Dashboard.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './dashboard.css'
+import Hours from '../components/Hours'
+import Staff from '../components/Staff'
+import Settings from '../components/Settings'
+import { useState } from 'react'
 
 const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState('')
+
   return (
       <div className="dash_main">
         <div className="nav">
@@ -9,16 +15,19 @@ const Dashboard = () => {
             <h1>Simplicit</h1>
           </div>
           <div className="tab_links">
-            <a href="#">HOURS MANAGEMENT</a>
-            <a href="#">STAFF MANAGEMENT</a>
-            <a href="#">ACCOUNT SETTINGS</a>
+            <a href="#" onClick={() => {setActiveTab('hours')}}>HOURS MANAGEMENT</a>
+            <a href="#" onClick={() => {setActiveTab('staff')}}>STAFF MANAGEMENT</a>
+            <a href="#"onClick={() => {setActiveTab('settings')}}>ACCOUNT SETTINGS</a>
           </div>
           <div className="sign_out">
             <button>SIGN OUT</button>
           </div>
         </div>
         <div className="content">
-
+        {activeTab === 'hours' ? <Hours /> 
+        : activeTab === 'staff' ? <Staff /> 
+        : activeTab === 'settings' ? <Settings />
+        : "Nothing Selected"}
         </div>
       </div>
   )
