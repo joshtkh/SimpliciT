06d7a04c7dc801cbdf00e4edd3603d43d02e685c
Josh H <josh.terance@gmail.com>
2023-04-17 20:12:47 -0400

    moved login controller logic into its own file

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -2,45 +2,9 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const prisma = require('../config/db');
-require('dotenv').config();
-
+const loginController = require('../controllers/login.controller');
 const jwtSecret = process.env.JWT_SECRET;
-console.log(jwtSecret);
 
-router.post('/', async (req, res) => {
-  console.log("REQ BODY:", req.body);
-  const { employeeId, password, isManager } = req.body;
-
-  // Check if user exists
-  const user = await prisma.employee.findUnique({
-    where: {
-      EmployeeID: employeeId,
-    },
-    select:{
-        EmployeeID: true,
-        FirstName: true,
-        LastName: true,
-        Password: true
-    }
-  });
-    // console.log(user.FirstName);
-  if (!user) {
-    return res.status(401).json({ error: 'Invlid EmployeeID or password!' });
-  }
-
-  //comparing plain text password
-  if(user.Password !== password){
-    return res.status(401).json({error: 'Invalid password'});
-  }
-
-  // Create and sign a JWT token
-  const token = jwt.sign({ id: user.EmployeeID, name: user.FirstName }, jwtSecret, {
-    expiresIn: '1d'
-  });
-  
-  // Return token to client
-  return res.status(200).json({ token });
-  
-});
+router.post('/', loginController.login);
 
 module.exports = router;
