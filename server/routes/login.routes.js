1755dc6847c133638287689db386e1dfd0371d69
Josh H <josh.terance@gmail.com>
2023-04-17 20:17:26 -0400

    removed old imports

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const prisma = require('../config/db');
 const loginController = require('../controllers/login.controller');
-const jwtSecret = process.env.JWT_SECRET;
 
 router.post('/', loginController.login);
 
